4abc3d2ba12ed22cabffcca8d5b8befcb7469c07
Deven Bhooshan <devenbhooshan@gmail.com>
2021-06-04 02:54:53 +0530

    add a details sections

--- src/js/app.js
+++ src/js/app.js
@@ -1,14 +1,16 @@
 const todoInput = document.querySelector('.todo-input');
 const todoList = document.querySelector('.todo-list');
 
-let createElements = (value) => {
+let showAutoExpand = true;
+
+let createElements = (item) => {
     // create div container
     const todoDiv = document.createElement('div');
     todoDiv.classList.add('todo')
 
     // create element li 
     const newTodo = document.createElement('li');
-    newTodo.innerText= value;
+    newTodo.innerText= item.title;
     newTodo.classList.add('todo-item');
 
     // make newTodo child from todoDiv
@@ -16,14 +18,28 @@ let createElements = (value) => {
 
     // create button check
     const checkButton = document.createElement('button');
-    checkButton.classList.add('fas', 'fa-check', 'check-btn') // error if use space, so split class with '', 
-    
+    checkButton.classList.add('fas', 'fa-check', 'check-btn');
     todoDiv.appendChild(checkButton);
     todoDiv.appendChild(newTodo);
+    const detailPanel = document.createElement('div');
+    // create button check
+    const expandButton = document.createElement('button');
 
+    if(showAutoExpand) {
+        detailPanel.classList.add('detail-panel', 'show');
+        expandButton.classList.add('fas', 'fa-minus', 'expand-btn');
+        showAutoExpand = false;
+    }else {
+        detailPanel.classList.add('detail-panel');
+        expandButton.classList.add('fas', 'fa-plus', 'expand-btn');
+    }
+    detailPanel.innerText = item.description;
+
+    todoDiv.appendChild(expandButton);
 
     // make todoDiv child from todoList
     todoList.appendChild(todoDiv);
+    todoList.appendChild(detailPanel);
 }
 
 // function check or delete what user click
@@ -36,6 +52,21 @@ let checkOrDelete = (e) => {
         const todo = item.parentElement;
         todo.classList.toggle('check');
     }
+    if (item.classList.contains("fa-plus")){
+        item.classList.add('fa-minus');
+        const panel = item.parentElement.nextSibling;
+        item.classList.remove('fa-plus');
+        panel.classList.add('show');
+        return
+    }
+
+    if (item.classList.contains("fa-minus")){
+        const panel = item.parentElement.nextSibling;
+        item.classList.add('fa-plus');
+        item.classList.remove('fa-minus');
+        panel.classList.remove('show');
+        return
+    }
 }
 
 let createSection = (section) => {
@@ -45,7 +76,7 @@ let createSection = (section) => {
     sectionTitle.innerText = section.title;
     todoList.appendChild(sectionTitle);
     section.items.forEach(item => {
-        createElements(item.title);
+        createElements(item);
     })
 }
 
